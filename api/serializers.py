88dc23d98b365edccbe370c94b88f73c1fdfdb11
João Fagundes <joaovictordmf@gmail.com>
2020-06-05 17:31:51 -0300

    create statistics serializer and view

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,15 @@ class SalarySerializer(serializers.ModelSerializer):
         fields = ('id', 'date', 'value', 'discounts', 'user')
 
 class UserSerializer(serializers.ModelSerializer):
-    salaries = SalarySerializer(many=True)
+    salaries = SalarySerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = ('id', 'name', 'cpf', 'date_of_birth', 'salaries')
         cpf = serializers.CharField(max_length=14)
+
+class StatisticSerializer(serializers.Serializer):
+    avg_salaries = serializers.FloatField()
+    avg_discounts = serializers.FloatField()
+    max_salary = serializers.FloatField()
+    min_salary = serializers.FloatField()
+
--- api/views.py
+++ api/views.py
@@ -1,11 +1,26 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from django.db.models import Avg, Max, Min
 from api.models import User, Salary
-from api.serializers import UserSerializer, SalarySerializer
+from api.serializers import UserSerializer, SalarySerializer, StatisticSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.select_related()
     serializer_class = UserSerializer
 
 class SalaryViewSet(viewsets.ModelViewSet):
     queryset = Salary.objects.all()
     serializer_class = SalarySerializer
+
+class StatisticViewSet(viewsets.ViewSet):
+    serializer_class = StatisticSerializer
+
+    def list(self, request):
+        query = Salary.objects.aggregate(avg_salaries=Avg('value'),
+                                         avg_discounts=Avg('discounts'),
+                                         max_salary=Max('value'),
+                                         min_salary=Min('value'))
+
+        return Response(StatisticSerializer(query).data)
+
+

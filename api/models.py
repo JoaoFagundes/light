c4479a99ee6bf38a2f2e81bba495ea9e49c4180f
João Fagundes <joaovictordmf@gmail.com>
2020-06-05 11:52:17 -0300

    create cpf format and validation, change date of birth field

--- api/models.py
+++ api/models.py
@@ -1,11 +1,28 @@
+import re
 from django.db import models
-
-# Create your models here.
+from django.core.validators import RegexValidator
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    cpf = models.CharField(max_length=14)
-    birthday = models.DateField()
+    cpf = models.CharField(max_length=14,
+                           validators=[
+                               RegexValidator(r'^((\d){3}.?){2}(\d){3}-?(\d){2}$')
+                           ])
+    date_of_birth = models.DateField()
+
+    def save(self, *args, **kwargs):
+        formatted_cpf = re.search("^((\d){3}.){2}(\d){3}-(\d){2}$", self.cpf)
+        print(formatted_cpf)
+        if not formatted_cpf:
+            self.cpf = '{0}.{1}.{2}-{3}'.format(self.cpf[:3],
+                                                self.cpf[3:6],
+                                                self.cpf[6:9],
+                                                self.cpf[9:])
+
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '{}, CPF: {}'.format(self.name, self.cpf)
 
 class Salary(models.Model):
     date = models.DateField()
--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
-from api.models import Company, Result
+from api.models import User, Salary
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'cpf', 'birthday')
+        fields = ('id', 'name', 'cpf', 'date_of_birth')
+        cpf = serializers.CharField(max_length=14)
 
 class SalarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Salary
         fields = ('id', 'date', 'value', 'discounts', 'user')
+
